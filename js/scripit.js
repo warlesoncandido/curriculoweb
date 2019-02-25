94a2416ed803ec5c0b00962c1da75fa16843e265
Warleson Candido <warllesoncandido@gmail.com>
2019-02-25 12:15:28 -0300

    Curriculo Online

---
+++ js/scripit.js
@@ -0,0 +1,27 @@
+document.querySelector("#home").addEventListener("click",function(){
+
+    document.querySelector("#article").innerHTML="<h3>Olá meu nome e Warleson Candido da Silva sou natural de contagem e atualmente moro em Betim , desde pequeno sou envolvido com computação ,influenciado por toda minha familia ,desde cedo busquei conhecimento para que eu  estivese preparado para adentrar a uma Universidade e este dia chegouconclui meu curso na Universidade do Parana "+"<b>(UNOPAR)</b>"+" de ANALISE E DESENVOLVIMENTO DE SISTEMAS e agora estou  em busca de um novo emprego na area de Front-End.</h3>";
+
+});
+document.querySelector("#formacao").addEventListener("click",function(){
+
+    document.querySelector("#article").innerHTML="<h2>Universidade do Parana</h2><h4>Superior Analise e Desenvolvimento de Sistemas - concluso em 2018</h4><h2>Curso Front-End Udemy</h2><h4>Conclusão prevista para 2019</h4><h2>JavaScripit Udemy</h2><h4> Conclusão prevista para 2019</h4>";
+
+});
+document.querySelector("#experiencia").addEventListener("click",function(){
+
+    document.querySelector("#article").innerHTML="<h2>McDonalds Bigfoods</h2><h4> Atendente junior I 0-04/2010 a 05/2010</h4><h2>Almaviva do Brasil</h2><h4>Operador de Telemarketing - 11/2010 a 10/2011 </h4><h2>Promef</h2><h4>Auxiliar de serviços gerais - 01/2012 a 10/2012</h4><h2>Telemont</h2><h4>Instalador e Reparador de Linhas - 11/2012 a 07/2018</h4>";
+
+});
+  
+document.querySelector("#contato").addEventListener("click",function(){
+
+    document.querySelector("#article").innerHTML="<h2>Telefone</h2><h4>(31)3592-9231</h4><h2>Celular</h2><h4>(31)99221-9820</h4><h2>Email</h2><h4>warllesoncandido@gmail.com / warlesoncandido@hotmail.com</h4>";
+
+});
+  
+  
+
+    
+
+
